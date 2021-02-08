6b7b7fa6c70100a233de3082e4557d8c0cc22354
jonasewald <jonas.ewald@gmail.com>
2021-02-08 11:23:31 -0300

    first commit

--- bootstrap/projeto-galeria/src/js/core/plugins/cityButtons.js
+++ bootstrap/projeto-galeria/src/js/core/plugins/cityButtons.js
@@ -15,29 +15,29 @@ function filterByCity(city) {
     })
 }
 
-const cityButtons = $('[wm-city-buttons]')
-const cities = new Set
-$('[wm-city]').each(function (i, e) {
-    cities.add($(e).attr('wm-city'))
-})
+$.fn.cityButtons = function () {
+    const cities = new Set
+    $('[wm-city]').each(function (i, e) {
+        cities.add($(e).attr('wm-city'))
+    })
 
-const btns = Array.from(cities).map(city => {
-    const btn = $('<button>')
-        .addClass(['btn', 'btn-info']).html(city)
-    //btn.click(e => filterByCity(city))
-    //return btn
-})
+    const btns = Array.from(cities).map(city => {
+        const btn = $('<button>')
+            .addClass(['btn', 'btn-info']).html(city)
+        btn.click(e => filterByCity(city))
+        return btn
+    })
 
-const btnAll = $('<button>')
-    .addClass(['btn', 'btn-info', 'active']).html('Todas')
-btnAll.click(e => filterByCity(null))
-btns.push(btnAll)
+    const btnAll = $('<button>')
+        .addClass(['btn', 'btn-info', 'active']).html('Todas')
+    btnAll.click(e => filterByCity(null))
+    btns.push(btnAll)
 
-const btnGroup = $('<div>').addClass(['btn-group'])
-btnGroup.append(btns)
+    const btnGroup = $('<div>').addClass(['btn-group'])
+    btnGroup.append(btns)
 
-$(this).html(btnGroup)
-return this
-
+    $(this).html(btnGroup)
+    return this
+}
 
 $('[wm-city-buttons]').cityButtons()
